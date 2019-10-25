a2e3cda24b4823dedfa41ec3f58779f86479364a
nametake <nametake.kyarabuki@gmail.com>
2019-10-26 00:03:08 +0900

    Added pair error

--- markerr.go
+++ markerr.go
@@ -13,19 +13,6 @@ func Mark(err error, marker string) error {
 	}
 }
 
-type markErr struct {
-	err    error
-	marker string
-}
-
-func (e *markErr) Error() string {
-	return fmt.Sprintf("%s: %v", e.marker, e.err)
-}
-
-func (e *markErr) Unwrap() error {
-	return e.err
-}
-
 func TakeMarker(err error) (string, error) {
 	for err != nil {
 		m, ok := err.(*markErr)
@@ -44,6 +31,61 @@ func TakeMarker(err error) (string, error) {
 	return "", nil
 }
 
+func Pair(main, sub error) error {
+	return &pairErr{
+		main: main,
+		sub:  sub,
+	}
+}
+
+func TakePair(err error) (error, error) {
+	for err != nil {
+		p, ok := err.(*pairErr)
+		if ok {
+			return p.main, p.sub
+		}
+
+		unwrap, ok := err.(unwrapper)
+		if !ok {
+			break
+		}
+
+		err = unwrap.Unwrap()
+	}
+
+	return nil, nil
+}
+
 type unwrapper interface {
 	Unwrap() error
+}
+
+var (
+	_ unwrapper = (*markErr)(nil)
+	_ unwrapper = (*pairErr)(nil)
+)
+
+type markErr struct {
+	err    error
+	marker string
+}
+
+func (e *markErr) Error() string {
+	return fmt.Sprintf("%s: %v", e.marker, e.err)
+}
+
+func (e *markErr) Unwrap() error {
+	return e.err
+}
+
+type pairErr struct {
+	main, sub error
+}
+
+func (e *pairErr) Error() string {
+	return fmt.Sprintf("%v: %v", e.sub, e.main)
+}
+
+func (e *pairErr) Unwrap() error {
+	return e.main
 }
